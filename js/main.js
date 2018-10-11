adcdcb81313f662a1f781b78ad22f9b969137997
LilliamHaro <qualia950@gmail.com>
2018-10-11 13:57:46 -0500

    colors

--- js/main.js
+++ js/main.js
@@ -4,13 +4,9 @@ $(document).ready(function(){
     $('.label-nav').toggleClass('label-expand');
     $('.nav-content').toggleClass('nav-content-expand')
     $('.nav-content ul li a').toggleClass('opacity-1')
-
     $('.sp-1').toggleClass('sp-cross-1')
     $('.sp-2').toggleClass('sp-cross-2')
-    $('.sp-3').toggleClass('sp-cross-3')
-
-   
-    
+    $('.sp-3').toggleClass('sp-cross-3') 
   })
 
 })
