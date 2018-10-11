e132e51ac7569b2434c5c5e4241a8956b902d74d
LilliamHaro <qualia950@gmail.com>
2018-10-11 12:59:22 -0500

    cross

--- js/main.js
+++ js/main.js
@@ -1,10 +1,15 @@
 $(document).ready(function(){
 
   $('.hamburguer-button').on('click',function(){
-    $('.label-nav').toggleClass('label-expand')
-    setTimeout(() => {
-      $('.nav-content').toggle('slow')
-    }, 800);
+    $('.label-nav').toggleClass('label-expand');
+    $('.nav-content').toggleClass('nav-content-expand')
+    $('.nav-content ul li a').toggleClass('opacity-1')
+
+    $('.sp-1').toggleClass('sp-cross-1')
+    $('.sp-2').toggleClass('sp-cross-2')
+    $('.sp-3').toggleClass('sp-cross-3')
+
+   
     
   })
 
