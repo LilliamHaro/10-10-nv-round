589f2bea4cb289458618590e9bb99679d50a7367
LilliamHaro <qualia950@gmail.com>
2018-10-11 15:52:40 -0500

    colors-2

--- js/main.js
+++ js/main.js
@@ -1,12 +1,28 @@
 $(document).ready(function(){
 
-  $('.hamburguer-button').on('click',function(){
-    $('.label-nav').toggleClass('label-expand');
-    $('.nav-content').toggleClass('nav-content-expand')
-    $('.nav-content ul li a').toggleClass('opacity-1')
-    $('.sp-1').toggleClass('sp-cross-1')
-    $('.sp-2').toggleClass('sp-cross-2')
-    $('.sp-3').toggleClass('sp-cross-3') 
+  $('.nav-in').on('click',function(){
+    $('.label-nav').addClass('label-expand');
+    $('.nav-content').addClass('nav-content-expand')
+    $('.sp-1').addClass('sp-cross-1')
+    $('.sp-2').addClass('sp-cross-2')
+    $('.sp-3').addClass('sp-cross-3') 
+    $('.hamburguer-button-2').addClass('index-cross');
+    setTimeout(function(){
+      $('.nav-content ul li a').addClass('opacity-1')
+    },200)
+    setTimeout(function(){
+      $('.nav-content ul li a').addClass('opacity-1')
+    },500)
+  })
+
+  $('.nav-out').on('click',function(){
+    $('.label-nav').removeClass('label-expand');
+    $('.nav-content').removeClass('nav-content-expand')
+    $('.nav-content ul li a').removeClass('opacity-1')
+    $('.sp-1').removeClass('sp-cross-1')
+    $('.sp-2').removeClass('sp-cross-2')
+    $('.sp-3').removeClass('sp-cross-3') 
+    $('.hamburguer-button-2').removeClass('index-cross');
   })
 
 })
